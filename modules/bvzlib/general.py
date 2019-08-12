686fb3baf4933abf3261854a970984490054b270
bvz <bvz@c381.com>
2019-08-12 12:21:30 -0700

    Minor formatting change.

--- modules/bvzlib/general.py
+++ modules/bvzlib/general.py
@@ -20,6 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
 # ------------------------------------------------------------------------------
 def merge_lists_unique(list1, list2, case_sensitive=True):
     """
